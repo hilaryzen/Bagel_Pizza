91c77309d0eb700d46066080a6af210713bfecaa
Hilary Zen <hzen00@stuy.edu>
2020-05-03 16:05:40 -0400

    Import JSON functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,41 @@
 from flask import Flask, render_template, request
+import json
 app = Flask(__name__)
 
 co2Emissions = {}
+
+# year: anomaly
+# {1901: -0.15, 1902: -0.25, etc}
 globalTemp = {}
-usTemp = {}
+
+# year: value
+# {1901: 51.87, 1902: 51.59, etc}
+usTempValue = {}
+# year: anomaly
+# {1901: -0.15, 1902: -0.43, etc}
+usTempAnomaly = {}
 
 def importCSV(fileName):
     return 5
 
-def importJSON(fileName):
-    return 5
+def importUsTemp(fileName):
+    with open(fileName, 'r') as file:
+        data = json.loads(file.read())['data']
+        date = 1901
+        for i in data:
+            if int(i) / 100 >= 1901 and int(i) / 100 <= 2001:
+                usTempValue[date] = float(data[i]['value'])
+                usTempAnomaly[date] = float(data[i]['anomaly'])
+                date += 1
+
+def importGlobalTemp(fileName):
+    with open(fileName, 'r') as file:
+        data = json.loads(file.read())['data']
+        date = 1901
+        for i in data:
+            if int(i) >= 1901 and int(i) <= 2000:
+                globalTemp[date] = float(data[i])
+                date += 1
 
 @app.route("/")
 def hello_world():
@@ -17,7 +43,7 @@ def hello_world():
 
 @app.route("/temperature")
 def temperature():
-    return render_template("temp.html")
+    return render_template("temp.html", usTempValue = usTempValue, usTempAnomaly = usTempAnomaly, globalTemp = globalTemp)
 
 @app.route("/emissions")
 def emissions():
@@ -29,4 +55,8 @@ def compare():
 
 if __name__ == "__main__":
     app.debug = True
+    importUsTemp('app/static/us_temp.json')
+    #print(usTempValue)
+    #print(usTempAnomaly)
+    importGlobalTemp('app/static/global_temp.json')
     app.run()
